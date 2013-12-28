26c3cf9c1317ad576724e3aff076f399b3ba8b1e
ichaostiz <ichaostiz@b9bcf5e5-cf2b-a2cc-1bdc-c301ae53e8f4>
2013-12-28 14:07:16 +0000

    (Almost) Complete Histogram (Pet Speech Left)

--- trunk/BestProjectEver/src/histogramDraw/HistogramPanel.java
+++ trunk/BestProjectEver/src/histogramDraw/HistogramPanel.java
@@ -13,7 +13,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
